22a5096ff00bafa882542cfcd7b83eb0565e2fa7
alexisraAlex <alexisran2000@gmail.com>
2025-10-01 15:33:04 -0500

    parte 1 del taller terminado
    estos serían los últimos cambios que hacían falta

--- Main.java
+++ Main.java
@@ -6,8 +6,13 @@ public class Main {
 		
 		
 		Animal ani = new Animal("Tipo de animal", 4);
-		System.out.println(ani.getNombre()+" "+"con edad de:"+ani.getEdad());
-
+			System.out.println(ani.getNombre()+" "+"con edad de:"+ani.getEdad());
+		Ave ave = new Ave ("Chipi", 2, "Perico");
+			System.out.println("Este pájaro se llama:"+" "+ave.getNombre()+" "+"con edad de"+" "+ave.getEdad()+" "+"Y su raza es"+" "+ave.getRazaAve());
+		Gato gato = new Gato ("Michi", 5, "Callejero");
+			System.out.println("Este gato se llama:"+" "+gato.getNombre()+" "+"con edad de"+" "+gato.getEdad()+" "+"Y su raza es"+" "+gato.getRazagato());
+		Perro perro = new Perro("Firulais", 6, "Callejero");
+			System.out.println("Este perro se llama:"+" "+perro.getNombre()+" "+"con edad de"+" "+perro.getEdad()+" "+"Y su raza es"+" "+perro.getRazaperro());
 	}
 
 }
